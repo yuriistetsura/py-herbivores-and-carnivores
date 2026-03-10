9f5f656d223c4e8c035476674d80ffdf334499f7
YuriiStetsura <yurii.stetsura@playsource.tech>
2026-03-10 18:13:24 +0200

    implement Herbivores and carnivores

--- app/main.py
+++ app/main.py
@@ -1,1 +1,39 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.hidden = hidden
+        self.health = health
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Herbivore) -> None:
+        if not isinstance(herbivore, Herbivore):
+            return
+        if herbivore.hidden:
+            return
+
+        herbivore.health -= 50
+
+        if herbivore.health <= 0:
+            herbivore.health = 0
+            Animal.alive.remove(herbivore)
